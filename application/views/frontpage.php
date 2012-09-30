ca08d37b52833a18f37a95cc1e91d9b650e37641
Steven Lu <tacticalazn@gmail.com>
2012-09-30 16:57:24 -0400

    Added documentation.

--- application/views/frontpage.php
+++ application/views/frontpage.php
@@ -1,6 +1,66 @@
 <div class="container">
+	<div id="buttons">
+		<div class="btn btn-primary btn-large">Login</div>
+		<div class="btn btn-large">Register</div>
+	</div>
    <div class="hero-unit">
       <h2>Simple Notification Service</h2>
-      <p>The simple notification service allows you to send notifications from any of your applications easily to your devices. More information will be available as this service is being developed.</p>
+      <p><u>Simple Notification Service</u> or <strong>SNS</strong> allows you to send any notification you want straight to your iOS devices. This service was built from the ground up with speed and scalability in mind. It's easy to use and has a full fledged API to interact with the service. The best part, it's completely free to use.</p>
+   </div>
+   <h2>API Documentation</h2>
+   <div class="well">
+   	<h4>POST /api/notification</h4>
+   	<p>As easy as that, <strong>POST</strong> to that endpoint to send a message to a device.</p>
+
+   	<h5>Inputs</h5>
+   	<table class="table table-bordered table-hover">
+   		<thead>
+   			<tr>
+   				<th>Input</th>
+   				<th>Required</th>
+   				<th width="60%">Description</th>
+   			</tr>
+   		</thead>
+   		<tbody>
+   			<tr>
+   				<td>key</td>
+   				<td><span class="label label-important">required</span></td>
+   				<td>The API key to post the message to, this string is typically 32 characters long.</td>
+   			</tr>
+   			<tr>
+   				<td>subject</td>
+   				<td><span class="label label-important">required</span></td>
+   				<td>Message subject string, can be anything you like at a maximum of 128 characters.</td>
+   			</tr>
+   			<tr>
+   				<td>message</td>
+   				<td><span class="label label-important">required</span></td>
+   				<td>The actual message, can be anything, as long as you like.</td>
+   			</tr>
+   		</tbody>
+   	</table>
+
+   	<h5>Success Response</h5>
+   	<code>{"success":"Notification added."}</code>
+
+   	<h5>Error Responses</h5>
+   	<table class="table table-bordered table-hover">
+   		<thead>
+   			<tr>
+   				<th>Code</th>
+   				<th width="80%">Description</th>
+   			</tr>
+   		</thead>
+   		<tbody>
+   			<tr>
+   				<td>100</td>
+   				<td>Inputs are missing.</td>
+   			</tr>
+   			<tr>
+   				<td>300</td>
+   				<td>API Key is not valid.</td>
+   			</tr>
+   		</tbody>
+   	</table>
    </div>
 </div>
