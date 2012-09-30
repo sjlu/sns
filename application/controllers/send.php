02df184345938edc131484a7aa48ba484f40bf7f
Steven Lu <tacticalazn@gmail.com>
2012-09-30 17:49:09 -0400

    Added send endpoint.

--- application/controllers/send.php
+++ application/controllers/send.php
@@ -4,7 +4,13 @@ class Send extends Web_Controller {
 
 	function index()
 	{
-		
+		$this->load->library('apns');
+		$this->load->model('notifications_model');
+
+		$notifications = $this->notifications_model->dequeue(1);
+
+		foreach ($notifications as $n)
+			$this->apns->send_message($n['push_key'], $n['subject'].'-'.$n['message']);
 	}
 
 }
