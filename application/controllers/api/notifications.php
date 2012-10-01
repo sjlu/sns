f7d0d5ed36d4c730009cd9e73cc7f4dbfa64d9da
Steven Lu <tacticalazn@gmail.com>
2012-10-01 17:19:57 -0400

    Can grab notifications by unique DUID.

--- application/controllers/api/notifications.php
+++ application/controllers/api/notifications.php
@@ -4,9 +4,13 @@ class Notifications extends API_Controller {
 
 	function index_get()
 	{
-		$user = $this->require_session(); 
+		$duid = $this->get('duid');
+
+		if (empty($duid))
+			$this->error_response(100);
+
 		$this->load->model('notifications_model');
-		$this->response($this->notifications_model->get_notifications_by_user($user));
+		$this->response($this->notifications_model->get_notifications_by_duid($duid));
 	}
 
 }
