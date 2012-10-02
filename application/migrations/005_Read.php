27706bb164801f5d02068bf9286da6d4e2fa72df
Steven Lu <tacticalazn@gmail.com>
2012-10-02 12:15:48 -0400

    Added migration.

---
+++ application/migrations/005_Read.php
@@ -0,0 +1,24 @@
+<?php
+
+class Migration_read extends CI_Migration {
+
+	function up() 
+	{
+		$fields = array(
+			'read' => array(
+				'type' => 'INT',
+				'constraint' => 1,
+				'unsigned' => TRUE,
+				'default' => 0
+			)
+		);
+
+		$this->dbforge->add_column('notifications', $fields);
+	}
+
+	function down()
+	{
+		$this->dbfoge->drop_column('notifications', 'read');
+	}
+
+}
